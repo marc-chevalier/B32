97048f6624a19eaea54649309c91d6240925df7b
SINewton <marc.chevalier@ens-lyon.org>
2015-02-12 11:27:23 +0100

    Commit initial

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if(argc != 3)
+    {
+        cerr<<"Utilisation : B32 keys plaintext"<<endl;
+        return EXIT_FAILURE;
+    }
+
+
+
+    return 0;
+}
